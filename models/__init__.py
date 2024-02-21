adea0da728b236a31d9ae2575880825624e03fc9
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-21 18:00:17 +0300

    updated magic method

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """__init__ majic method for models directory"""
-from models.engine.file_storage import FileStorage
+from .file_storage import FileStorage
 
 
 storage = FileStorage()
