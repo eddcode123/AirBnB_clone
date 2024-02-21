1badafcae105fac96ccc434fb3b12f22ede80297
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-21 16:52:52 +0300

    updated BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes a BaseModel instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+
         self.updated_at = datetime.now()
         # Update instance attributes if provided in kwargs
         if kwargs:
+            # iterate over each attribiute
             for key, value in kwargs.items():
                 if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the object"""
@@ -32,6 +37,6 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
         # Convert 'created_at' and 'updated_at' to ISO format strings
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        obj_dict['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         return obj_dict
